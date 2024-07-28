4f31475368e0275119b648ab468d2d444e890410
Adesugba Samuel <adeolasam20@gmail.com>
2024-07-28 11:12:57 +0100

    update

--- src/constants/secrets.const.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-const secrets = {
-    port: process.env.PORT,
-    jwtSecret: process.env.JWT_SECRET,
-};
-exports.default = secrets;
