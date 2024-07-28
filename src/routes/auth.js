f51a89d731955fe4e97c0e95a4fa6a4a41781157
Adesugba Samuel <adeolasam20@gmail.com>
2024-07-28 11:20:18 +0100

    update

--- src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRoutes = void 0;
-const express_1 = require("express");
-const auth_1 = require("../controllers/auth");
-const errorHandler_1 = require("../handlers/errorHandler");
-exports.authRoutes = (0, express_1.Router)();
-exports.authRoutes.post("/signup", (0, errorHandler_1.errorHandler)(auth_1.signup));
-exports.authRoutes.post("/login", (0, errorHandler_1.errorHandler)(auth_1.login));
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rootRouter = void 0;
-const express_1 = require("express");
-const auth_1 = require("./auth");
-const post_1 = require("./post");
-exports.rootRouter = (0, express_1.Router)();
-exports.rootRouter.use("/auth", auth_1.authRoutes);
-exports.rootRouter.use("/post", post_1.postRoutes);
